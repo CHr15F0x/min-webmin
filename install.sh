c5ca8859b546f3367f8e236c2b369e72c5b48255
Krzysztof Lis <klis33@gmail.com>
2020-11-08 16:25:43 +0100

    Minimal webmin installation script ok

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #/usr/bin/env bash
 
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Please run as root"
+    exit 1
+fi
+
 WEBMIN_PACKAGE=webmin-1.960-minimal.tar.gz
 WEBMIN_SRC=${PWD}/webmin-1.960
 SETUP_PRE=${PWD}/setup-pre.sh
-
 INSTALL_DIR=/writable/webmin
+
+rm -rf ${WEBMIN_SRC}
+rm -rf ${INSTALL_DIR}
 
 tar -xvzf ${WEBMIN_PACKAGE}
 
@@ -12,4 +19,4 @@ cp ${SETUP_PRE} ${WEBMIN_SRC}
 
 cd ${WEBMIN_SRC}
 
-setup.sh ${INSTALL_DIR}
+${PWD}/setup.sh ${INSTALL_DIR}
--- setup-pre.sh
+++ setup-pre.sh
@@ -1,2 +1,8 @@
 config_dir=/writable/webmin/etc
 var_dir=/writable/webmin/log
+perl=/usr/bin/perl
+port=10000
+login="admin"
+password="admin"
+ssl=1
+atboot=1
